17f8840fdf980b6d3b6c6df910baf7148d1b153e
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2022-10-04 18:16:52 +0400

    Shop

--- Frontend/src/components/Menu/MenuItems.jsx
+++ Frontend/src/components/Menu/MenuItems.jsx
@@ -2,7 +2,7 @@ export const items=[
     {itemText:"HOME",itemLink:"/"},
     {itemText:"SHOP",itemLink:"/shop"},
     {itemText:"SALE",itemLink:"/sale"},
-    {itemText:"FEATURES",itemLink:"/features"},
+    {itemText:"SHOP",itemLink:"/features"},
     {itemText:"BLOG",itemLink:"/blog"},
     {itemText:"ABOUT",itemLink:"/about"},
     {itemText:"CONTACT",itemLink:"/contact"},
